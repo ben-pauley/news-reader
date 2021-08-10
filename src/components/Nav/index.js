6156ae10e2738e8c3e2cb5cdbdb37fd94c1162da
Ben <pauleybj@gmail.com>
2021-08-10 13:26:34 +0100

    added nav component

---
+++ src/components/Nav/index.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <h1>News Reader</h1>
+      <div className="links" style={{ marginBottom: "20px" }}>
+        <Link to="/">Home</Link>
+        <Link to="/bookmarks">Bookmarks</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
